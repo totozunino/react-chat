d5f4e4c1c8441996da7520eca3ea73780240991e
totozunino <mathiaszunino@gmail.com>
2022-10-03 02:16:53 -0300

    chore: add env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA22RfKdYEXMvGIVYsmZv1waDxS750lpKc",
-  authDomain: "react-chat-59c76.firebaseapp.com",
-  databaseURL: "https://react-chat-59c76-default-rtdb.firebaseio.com",
-  projectId: "react-chat-59c76",
-  storageBucket: "react-chat-59c76.appspot.com",
-  messagingSenderId: "361496559948",
-  appId: "1:361496559948:web:583bf7455d1baee1254fe9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
